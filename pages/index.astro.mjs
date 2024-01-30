c2b1b61c691346b00c1a68114d3d3780cef83d20
lis531 <82408934+lis531@users.noreply.github.com>
2024-01-30 15:05:43 +0100

    Build project for branch: test

--- pages/index.astro.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('../chunks/pages/index_5c0147ee.mjs');
-
-export { page };
